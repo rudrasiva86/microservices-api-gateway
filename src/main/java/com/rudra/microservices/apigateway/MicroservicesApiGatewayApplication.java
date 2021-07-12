ad02dfa067f623184b28d22ebd92950df63edc38
rudrasiva86 <rudrasiva86@gmail.com>
2021-07-12 22:48:17 +0530

    first commit

---
+++ src/main/java/com/rudra/microservices/apigateway/MicroservicesApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.rudra.microservices.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesApiGatewayApplication.class, args);
+	}
+
+}
